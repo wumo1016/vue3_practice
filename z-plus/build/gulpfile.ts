2f2ced23e751f263cf089d954cc35126547ba7f0
wumo1016 <760478279@qq.com>
2021-10-27 21:51:59 +0800

    chore(z-plus-打包)

--- z-plus/build/gulpfile.ts
+++ z-plus/build/gulpfile.ts
@@ -1,6 +1,12 @@
 // 串行和并行
 import { parallel, series } from 'gulp'
 import { run, withTaskName } from './utils'
+import { genTypes } from './gen-types'
+import { outDir, zpRoot } from './utils/paths'
+
+const copySourceCode = () => async () => {
+  await run(`cp ${zpRoot}/package.json ${outDir}/package.json`)
+}
 
 export default series(
   withTaskName('clean', () => run('rm -rf ./dist')),
@@ -14,7 +20,8 @@ export default series(
       () => run('pnpm run build buildFullComponent') // 直接执行任务 buildFullComponent 执行命令时会调用rollup 我们给rollup传递参数buildFullComponent(这是一个任务名) 它就会执行
     ),
     withTaskName('buildComponent', () => run('pnpm run build buildComponent'))
-  )
+  ),
+  parallel(genTypes, copySourceCode())
 )
 
 export * from './full-component'
