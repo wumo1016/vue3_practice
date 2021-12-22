26a568cf9e7d160ecc7a36df15231a7d44a933f2
wumo1016 <760478279@qq.com>
2021-12-22 17:53:40 +0800

    chore

--- vue3_4/src/views/HomeView.vue
+++ vue3_4/src/views/HomeView.vue
@@ -1,9 +1,6 @@
+<template>home</template>
+
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
