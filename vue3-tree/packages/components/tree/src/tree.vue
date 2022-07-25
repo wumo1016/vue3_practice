1c5b1e771c726140f48bbdee8a03d0123f0fd3fd
wumo1016 <760478279@qq.com>
2022-07-25 20:04:59 +0800

    chore

--- vue3-tree/packages/components/tree/src/tree.vue
+++ vue3-tree/packages/components/tree/src/tree.vue
@@ -1,7 +1,70 @@
 <template>tree1</template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import { TreeOption, treeProps, TreeNode } from './tree'
+
 defineOptions({
   name: 'z-tree'
 })
+
+const props = defineProps(treeProps)
+console.log(props)
+
+// 格式化数据
+function createTree(data: TreeOption[], parent: TreeNode | null = null) {
+  function traversal(data: TreeOption[], parent: TreeNode | null = null) {
+    return data.map(node => {
+      const children = treeOptions.getChildren(node) || []
+      const treeNode: TreeNode = {
+        key: treeOptions.getKey(node),
+        label: treeOptions.getLabel(node),
+        children: [], // 默认为空
+        rawNode: node,
+        level: parent ? parent.level + 1 : 0,
+        // 判断节点是否自带isLeaf 如果自带了 以自带的为准，如果没有自带的则看一下有没有children属性
+        // 对 ||的增强操作  ?.  ??
+        isLeaf: node.isLeaf ?? children?.length == 0, // 是否是叶子节点
+        disabled: !!node.disabled // 添加disabled属性
+      }
+      if (children.length > 0) {
+        // 有孩子再去递归孩子，将其格式化成treeNode类型
+        treeNode.children = traversal(children, treeNode)
+      }
+      return treeNode
+    })
+  }
+  const result: TreeNode[] = traversal(data, parent)
+  return result
+}
+const treeOptions = createOptions(
+  props.keyField,
+  props.labelField,
+  props.childrenField
+)
+function createOptions(key: string, label: string, children: string) {
+  return {
+    getKey(node: TreeOption) {
+      return node[key] as string // 用户传递的key
+    },
+    getLabel(node: TreeOption) {
+      return node[label] as string // 用户传递label
+    },
+    getChildren(node: TreeOption) {
+      return node[children] as TreeOption[] // 用户传递的children获取孩子
+    }
+  }
+}
+
+// 我们将props.data 格式化后放到tree中
+const tree = ref<TreeNode[]>([])
+// 监控数据变化，调用格式化方法。 一上来先格式化一次
+watch(
+  () => props.data,
+  (data: TreeOption[]) => {
+    tree.value = createTree(data)
+    console.log(tree.value);
+  },
+  { immediate: true }
+)
 </script>
